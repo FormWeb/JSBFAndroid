9378314727345bdfd764849ad60ced7838aa86da
William <william.genard@bstorm.be>
2022-08-19 16:01:48 +0200

    add pendu

--- CorrectionPendu/script.js
+++ CorrectionPendu/script.js
@@ -1,12 +1,15 @@
 const hiddenWordHTML = document.querySelector("h3")
 const inputHTMl = document.querySelector("input")
-const testHTML = document.querySelector("button")
+const btnHTML = document.querySelector("button")
 const listWordHTML = document.querySelector("ul")
+const essaiHTML = document.querySelector("span")
 
 const words = ["maison", "appartement", "chat", "beluga"]
+const MAX_TRIES = 6
 
 let secretWord
 let hiddenWord
+let tries
 
 function initGame() {
     secretWord = words[Math.floor(Math.random() * words.length)]
@@ -15,15 +18,41 @@ function initGame() {
     hiddenWord = createHiddenWord(secretWord)
     console.log(hiddenWord)
 
+    tries = MAX_TRIES
+    essaiHTML.innerText = tries
+
     hiddenWordHTML.innerText = hiddenWord
 
     // A supprimer
 
-    for (const word of words) {
-        const itemHTML = document.createElement("li")
-        itemHTML.innerText = word
-        listWordHTML.appendChild(itemHTML)
+    // for (const word of words) {
+    //     const itemHTML = document.createElement("li")
+    //     itemHTML.innerText = word
+    //     listWordHTML.appendChild(itemHTML)
+    // }
+}
+
+function testChar() {
+    if (!checkIfInWord(inputHTMl.value, secretWord)) {
+        tries--
+        if (tries <= 0) {
+            let previousSecretWord = secretWord
+            initGame()
+            hiddenWordHTML.innerText = "Perdu ! Le mot était : " + previousSecretWord + ". Le nouveau mot est : " + hiddenWord
+        }
     }
+    else {
+        hiddenWord = generateHiddenWord(hiddenWord, secretWord, inputHTMl.value)
+        hiddenWordHTML.innerText = hiddenWord
+        if (hiddenWord === secretWord) {
+            initGame()
+            hiddenWordHTML.innerText = "Gagné ! Le nouveau mot est : " + hiddenWord
+        }
+    }
+
+    essaiHTML.innerText = tries
+    inputHTMl.value = ""
+    inputHTMl.focus()
 }
 
 function createHiddenWord(sWord) {
@@ -50,6 +79,16 @@ function generateHiddenWord(hWord, sWord, letter) {
     }
     return result
 }
+
+btnHTML.addEventListener("click", () => {
+    testChar()
+})
+
+inputHTMl.addEventListener("keyup", (e) => {
+    if (e.key === "Enter") {
+        testChar()
+    }
+})
 
 initGame()
 
